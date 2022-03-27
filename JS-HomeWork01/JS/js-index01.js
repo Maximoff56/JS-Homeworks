772222a0162fce321fdf91fa7dbd04b199b3a39b
Alexander1205 <alexander.maximov12@gmail.com>
2022-03-27 22:34:22 +0300

    first commit in new repo

---
+++ JS-HomeWork01/JS/js-index01.js
@@ -0,0 +1,25 @@
+// task1
+console.log("Задание 1:")
+let amount = 110000;
+let stopLine = 100000;
+let result = amount > stopLine ? `К оплате с учетом скидки 10%: ${amount*0.9} рублей`  : `Сумма к оплате без скидки: ${amount} рублей`;
+console.log(result);
+
+// task2
+console.log("Задание 2:")
+let x = 78;
+let y = ((x-(x%10))/10);
+let z = (x%10);
+console.log (x);
+console.log (y);
+console.log (z);
+console.log (y+z);
+
+// task4
+console.log("Задание 4:")
+let m = 12;
+let n = 56;
+m = m/0;
+n = -n/0;
+console.log(m);
+console.log(n);
